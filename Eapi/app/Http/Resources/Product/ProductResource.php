4e030991d4b96273f1201cb7c0faa3dc17cd6639
Suha <abdullahsuhaisik@gmail.com>
2018-12-05 01:09:52 +0300

    transform data

--- Eapi/app/Http/Resources/Product/ProductResource.php
+++ Eapi/app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,14 @@ class ProductResource extends JsonResource
             'name'=>$this->name,
             'description'=>$this->detail,
             'price'=>$this->price,
-            'stock'=>$this->stock,
-            'discount'=>$this->discount
+            'stock'=>$this->stock == 0 ? 'Out of stock' : $this->stock,
+            'totalPrice'=> round((1-($this->discount/100))*$this->price,2),
+            'discount'=>$this->discount,
+            'raiting'=>$this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2)
+            : 'no raiting yet',
+            'href' => [
+                'reviews' => route('reviews.index',$this->id)
+            ]
         ];
     }
 }
