8e000aedebfc9c2fa37ef7dcc85c1ad7c92d7a0e
Suha <abdullahsuhaisik@gmail.com>
2018-12-04 15:38:19 +0300

    Data seeding

--- Eapi/database/factories/ReviewFactory.php
+++ Eapi/database/factories/ReviewFactory.php
@@ -1,9 +1,19 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Model\Product;
 
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
-        //
+        //Laravel source factory
+        'product_id'=>function() {
+            return Product::all()->random();
+        },
+        'customer'=>$faker->name,
+        'review'=>$faker->paragraph,
+        'star'=>$faker->numberBetween(0,5),
+        'name'=>$faker->word,
+
+
     ];
 });
